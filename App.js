27caad1565c8e4e2530bd3a1554d1981b76fa509
keng <keng@pupasoft.com>
2017-09-02 16:55:10 +0700

    App js for test firebase get data

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import firebase from './firebase'
 export default class App extends React.Component {
+    constructor(){
+      super();
+      this.ref = null;
+    }
+    componentDidMount() {
+      this.ref = firebase.database().ref('todos');
+      this.ref.on('value', this.handleToDoUpdate);
+    }
+
+    componentWillUnmount() {
+      if (this.ref) {
+        this.ref.off('value', this.handleToDoUpdate);
+      }
+    }
+
+    handleToDoUpdate = (snapshot) => {
+      this.todos = snapshot.val() || {};
+      console.log('this.todos: ', this.todos);
+  
+      // this.setState({
+      //   todos: this.listView.cloneWithRows(this.todos),
+      // });
+    }
+
+    
   render() {
     return (
       <View style={styles.container}>
