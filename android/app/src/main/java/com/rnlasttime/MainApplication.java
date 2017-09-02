1565aed651d9fec1f2e71175de8254f153eddcc9
keng <keng@pupasoft.com>
2017-09-02 16:54:52 +0700

    setup react-native-firebase android

--- android/app/src/main/java/com/rnlasttime/MainApplication.java
+++ android/app/src/main/java/com/rnlasttime/MainApplication.java
@@ -11,6 +11,19 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+// Required package
+import io.invertase.firebase.RNFirebasePackage; // <-- Add this line
+// Optional packages - add as appropriate
+import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
+import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
+import io.invertase.firebase.crash.RNFirebaseCrashPackage; // Firebase Crash Reporting
+import io.invertase.firebase.database.RNFirebaseDatabasePackage; // Firebase Realtime Database
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // Firebase Cloud Messaging
+import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Performance
+import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -22,7 +35,17 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseAdMobPackage(),
+          new RNFirebaseAnalyticsPackage(),
+          new RNFirebaseAuthPackage(),
+          new RNFirebaseRemoteConfigPackage(),
+          new RNFirebaseCrashPackage(),
+          new RNFirebaseDatabasePackage(),
+          new RNFirebaseMessagingPackage(),
+          new RNFirebasePerformancePackage(),
+          new RNFirebaseStoragePackage()
       );
     }
   };
